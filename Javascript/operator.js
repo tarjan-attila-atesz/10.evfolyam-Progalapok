75c05193f021b9e6c8d0bdd17dccf1c505fe9729
tarjan-attila-atesz <tarjan.attila88@gmail.com>
2021-02-17 14:59:24 +0100

    commit

---
+++ Javascript/operator.js
@@ -0,0 +1,44 @@
+console.log("Hello World!");
+/////////////////////////////////////////////////////////////////////
+console.log('1' == 1);
+console.log('1' === 1); 
+console.log('1' !== 1);  
+console.log(1 + 1);  
+/////////////////////////////////////////////////////////////////////
+console.log('piros' + 'alma');
+console.log('1' + 1);
+console.log('Atesz ' + 12 + 'diákja');
+console.log('Atesz ' + 6 + 6 + 'diákja');
+console.log('Atesz ' + (6 + 6) + 'diákja');
+console.log(6 + 6 + ' tanítványa Atesznak');
+/////////////////////////////////////////////////////////////////////
+let i = 18;
+console.log(i);
+console.log(i++);
+console.log(i);
+console.log("++i:");
+console.log(++i);
+console.log(i);
+++i;
+i--;
+console.log(i);
+/////////////////////////////////////////////////////////////////////
+let name = "Atesz";
+let hello = `Szia ${name}!`;
+console.log(hello);
+/////////////////////////////////////////////////////////////////////
+console.log(typeof "apple");
+console.log(typeof 223);
+let fruit = "apple";
+console.log(typeof fruit);
+let u;
+console.log(typeof u);
+/////////////////////////////////////////////////////////////////////
+let result = 1 + 2 + "3" + 4 + 5;
+console.log(result);
+let result2 = 1 + 2 + "3" * 1 + 4 + 5;
+console.log(result2);
+var eletkor = 19;
+console.log(eletkor);
+var eletkor = 21;
+console.log(eletkor);
