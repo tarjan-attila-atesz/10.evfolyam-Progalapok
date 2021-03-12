2176a5e91efca0b000115504d665615c3e3bc30b
Tarjan Attila Atesz <tarjan.attila@sziszi.hu>
2021-03-12 23:02:48 +0100

    commit

--- Javascript/hello.js
+++ Javascript/hello.js
@@ -2,16 +2,20 @@
 function $(id) {
     return document.getElementById(id);
 }
+
 function nevbolUdvozles(nev) {
     return 'Hello ' + nev + '!';
 }
+
 function hello() {
     var nev = $('nev').value;
     var udvozles = nevbolUdvozles(nev);
     $('kimenet').innerHTML = udvozles;
 }
+
 function init() {
     //Eseménykezelők regisztrálása
     $('gomb').addEventListener('click', hello, false);
 }
 window.addEventListener('load', init, false);
+console.log("test")
