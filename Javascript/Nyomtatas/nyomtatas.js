53d8c7961798b0e71cb389401152be57f55e23e9
Tarjan Attila Atesz <tarjan.attila@sziszi.hu>
2021-03-16 10:30:02 +0100

    commit

---
+++ Javascript/Nyomtatas/nyomtatas.js
@@ -0,0 +1,10 @@
+function kalkulal(){
+    //Űrlapadatok
+
+    const papir=document.getElementById('papirtipus').value;
+    //Számítások
+
+    //Megjelenítés
+    document.getElementById('valasz').style.visibility = "visible";
+}
+
