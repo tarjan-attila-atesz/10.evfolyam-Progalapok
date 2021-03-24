1719f6f6a17dfffdb301736aa3caa79d0ccfec66
Tarjan Attila Atesz <tarjan.attila@sziszi.hu>
2021-03-24 13:21:50 +0100

    commit

--- Javascript/Nyomtatas/nyomtatas.js
+++ Javascript/Nyomtatas/nyomtatas.js
@@ -1,10 +1,18 @@
-function kalkulal(){
+function kalkulal() {
     //Űrlapadatok
-
-    const papir=document.getElementById('papirtipus').value;
+    const szelesseg = document.getElementById('szelesseg').value;
+    const magassag = document.getElementById('magassag').value;
+    const papir = document.getElementById('papirtipus').value;
     //Számítások
-
+    let terulet = Math.round((szelesseg * magassag) / 10000);
+    let koltseg = terulet * papir;
     //Megjelenítés
-    document.getElementById('valasz').style.visibility = "visible";
-}
 
+    document.getElementById('terulet').innerHTML = terulet;
+    document.getElementById('papir').innerHTML = papir;
+    document.getElementById('koltseg').innerHTML = koltseg;
+    document.getElementById('valasz').style.visibility = "visible";
+
+
+
+}
