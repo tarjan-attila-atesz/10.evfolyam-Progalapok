44aa8fff01aad1ecffa485c834ef54f54f177a4c
Tarjan Attila Atesz <tarjan.attila@sziszi.hu>
2021-04-20 09:25:00 +0200

    commit

--- Javascript/Utazasi_Iroda/kalkulal.js
+++ Javascript/Utazasi_Iroda/kalkulal.js
@@ -1,12 +1,22 @@
 function szamol() {
-    var letszam = 1;
-    var ejszaka = 1;
-    var orszag = 'b';
-    var fizetendo;
+    var letszam = parseInt(document.getElementById("letszam").value);
+    var ejszaka = parseInt(document.getElementById("ejszaka").value);
+    var orszag = parseInt(document.getElementById("orszag").value);
+    var ar = 5600;
+    if (orszag == "g") {
+        ar = 6200;
+    }
+    if (orszag == "h") {
+        ar = 5000;
+    }
+    if (orszag == "t") {
+        ar = 5800;
+    }
+
 
     //--számolás--
-      
+    var fizetendo = ejszaka * letszam * ar;
     //------------
-    document.getElementById('eredmeny').value = fizetendo+" Ft";
-    
+    document.getElementById('eredmeny').value = fizetendo.toLocaleString() + " Ft";
+
 }
